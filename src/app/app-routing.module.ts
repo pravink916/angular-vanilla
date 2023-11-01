9397b5a6b04227edcbef4c358b2cc851a0ecc64a
Pravin Kittappa <pravink916@gmail.com>
2023-11-01 12:11:12 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HelloWorldComponent,
+  },
+  {
+    path: 'helloWorld',
+    component: HelloWorldComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
